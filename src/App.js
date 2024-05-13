e2052f797ffdf3be44f2e4fdd66caf7a133792bc
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-05-13 16:13:32 -0300

    added css custom variables

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div> This is from App</div>
+    <div>     
+      <h1>This is a Display 1 Heading</h1>
+      <h2>This is a Heading 1</h2>
+      <h3>This is a Heading 2</h3>
+      <h4>This is a Heading 3</h4>
+      <p>This is a Paragraph 1</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit nibh, semper non pulvinar vitae, venenatis iaculis dolor. Sed dictum odio at augue porttitor egestas. Etiam hendrerit euismod cursus. Vestibulum semper mauris nec erat aliquet, vitae porttitor libero euismod. Pellentesque id ex gravida, euismod neque posuere, vulputate ex. Aenean ullamcorper odio purus, at sagittis enim tempor at. Sed finibus risus vel enim congue molestie. Mauris sagittis tempor libero, eu egestas neque placerat sed. Duis tempus tortor ac dolor gravida, non feugiat erat vestibulum.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit nibh, semper non pulvinar vitae, venenatis iaculis dolor. Sed dictum odio at augue porttitor egestas. Etiam hendrerit euismod cursus. Vestibulum semper mauris nec erat aliquet, vitae porttitor libero euismod. Pellentesque id ex gravida, euismod neque posuere, vulputate ex. Aenean ullamcorper odio purus, at sagittis enim tempor at. Sed finibus risus vel enim congue molestie. Mauris sagittis tempor libero, eu egestas neque placerat sed. Duis tempus tortor ac dolor gravida, non feugiat erat vestibulum.</p>
+      <p class="paragraph-2">This is a Paragraph 2
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit nibh, semper non pulvinar vitae, venenatis iaculis dolor. Sed dictum odio at augue porttitor egestas. Etiam hendrerit euismod cursus. Vestibulum semper mauris nec erat aliquet, vitae porttitor libero euismod. Pellentesque id ex gravida, euismod neque posuere, vulputate ex. Aenean ullamcorper odio purus, at sagittis enim tempor at. Sed finibus risus vel enim congue molestie. Mauris sagittis tempor libero, eu egestas neque placerat sed. Duis tempus tortor ac dolor gravida, non feugiat erat vestibulum.</p>
+      <button>BUTTON</button>
+      <a href="#">Hyperlink</a></div>
   );
 }
 
