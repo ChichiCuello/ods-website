11eba4175a0096b69361467cd48e5a21e1838f67
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-05-15 19:46:02 -0300

    hero section wheel

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import wheel from '../assets/wheel.png'
 import './inicio.css'
 
 export const Inicio = () => {
   return (<>
-<div className='bkg-img'></div>
-  <main>
-    <section className='section'>
-      <div>
-      <h1>This is a heading</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis magnam molestiae sequi maxime quo modi ipsa, id omnis aut illum voluptatibus iure assumenda sint doloribus soluta obcaecati. Modi, quasi veniam?</p>
-      <button className='button'>Know more</button>
-      </div>
-      {/* <img src={wheel} alt="logo ruede de colores de los ods" width={315} height={315}/> */}
+{/*   <main>
+    <section className='section hero-section'>
+
+      <img src={wheel} alt="logo ruede de colores de los ods" className='wheel-img'/>
     </section>
-  </main>
+  </main> */}
+    <div className='section'>
+      <div className='hero-section'>
+      <div className='hero-text'>
+        <h1>This is a heading</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis magnam molestiae sequi maxime quo modi ipsa, id omnis aut illum voluptatibus iure assumenda sint doloribus soluta obcaecati. Modi, quasi veniam?</p>
+        <button className='button'>Know more</button>
+        </div>
+      </div>
+      <div className="img"></div>
+    </div>
   </>
   )
 }
