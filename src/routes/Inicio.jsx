8fca6858b6105e4866d1e70e7025b56a0df4b61e
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-05-15 11:56:21 -0300

    hero secction first styles

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import wheel from '../assets/wheel.png'
+import './inicio.css'
 
 export const Inicio = () => {
-  return (
-    <div>
-      <h1>Inicio</h1>  {/* Add your desired content here */}
-      <p>This is the Inicio page. Add more content as needed.</p>
-  </div>
+  return (<>
+<div className='bkg-img'></div>
+  <main>
+    <section className='section'>
+      <div>
+      <h1>This is a heading</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis magnam molestiae sequi maxime quo modi ipsa, id omnis aut illum voluptatibus iure assumenda sint doloribus soluta obcaecati. Modi, quasi veniam?</p>
+      <button className='button'>Know more</button>
+      </div>
+      {/* <img src={wheel} alt="logo ruede de colores de los ods" width={315} height={315}/> */}
+    </section>
+  </main>
+  </>
   )
 }
