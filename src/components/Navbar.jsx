19e29ee1e0986d7d0072ba25b9ddb9cd00535218
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-05-15 10:52:50 -0300

    added closemenu functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,10 +27,15 @@ export const Navbar = () => {
     setIsSubmenuOpen(!isSubmenuOpen,); // Toggle menu for ods visibility
   };
 
-
+  const closeMenu = () => {
+    setIsMenuOpen(false); // Close the menu
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false); // Close the menu
+  };
   return (
     <nav className="navbar">
-      <Link to="/"><img {...logo}></img></Link>
+      <Link to="/ods-website"><img {...logo}></img></Link>
       <a onClick={toggleMenu} className="menu-toggle">
         <IconContext.Provider value={{ size: 32 }}>
           {isMenuOpen ? (
@@ -43,7 +48,7 @@ export const Navbar = () => {
 
       {isMenuOpen? (
           <ul className="popup-menu">
-            <li><Link to="/ods-website">Inicio</Link></li>
+            <li><Link to="/ods-website" onClick={closeMenu}>Inicio</Link></li>
             <li className="ods-list" onClick={toggleSubmenu}><a>Conoce los ODS 
            {isSubmenuOpen ? (
                 <FaAngleUp/> 
@@ -57,15 +62,15 @@ export const Navbar = () => {
               <ul className='submenu'>
                   {/* Render 17 list items in columns with automatic wrapping */}
                   {Array.from({ length: 17 }, (_, index) => (
-                    <li key={index + 1}><Link to={`ods-website/ods/${index + 1}`}>Ods {index + 1}</Link></li>
+                    <li key={index + 1}><Link to={`ods-website/ods/${index + 1}`} onClick={closeMenu}>Ods {index + 1}</Link></li>
                   ))}
               </ul>
             )}
-            <li><Link to="ods-website/recursos">Recursos</Link></li>
+            <li><Link to="ods-website/recursos" onClick={closeMenu}>Recursos</Link></li>
           </ul>
       ) : (
       <div className="big-menu">
-        <li className="paragraph-2"><Link to="/ods-website">Inicio</Link></li>
+        <li className="paragraph-2"><Link to="/ods-website" onClick={closeSubmenu}>Inicio</Link></li>
         <li className="paragraph-2 ods-list" onClick={toggleSubmenu}><a>Conoce los ODS 
            {isSubmenuOpen ? (
                 <FaAngleUp/> 
@@ -80,12 +85,12 @@ export const Navbar = () => {
                 <ul className='megamenu'>
                   {/* Render 17 list items in columns with automatic wrapping */}
                   {Array.from({ length: 17 }, (_, index) => (
-                    <li key={index + 1}><Link to={`ods-website/ods/${index + 1}`}>Ods {index + 1}</Link></li>
+                    <li key={index + 1}><Link to={`ods-website/ods/${index + 1}`} onClick={closeSubmenu}>Ods {index + 1}</Link></li>
                   ))}
               </ul>
               </>
             )}
-        <li className="paragraph-2"><Link to="ods-website/recursos">Recursos</Link></li>
+        <li className="paragraph-2"><Link to="ods-website/recursos" onClick={closeSubmenu}>Recursos</Link></li>
       </div>
       )}
     </nav>
