2748d511bb7830f4ef73fd880b90f97fab2ee05a
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-05-21 12:55:21 -0300

    changing styles

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './inicio.css'
+import wheel from '../assets/wheel.png'
 
 export const Inicio = () => {
   return (<>
@@ -9,15 +10,15 @@ export const Inicio = () => {
       <img src={wheel} alt="logo ruede de colores de los ods" className='wheel-img'/>
     </section>
   </main> */}
-    <div className='section'>
+    <div className='hero-main'>
       <div className='hero-section'>
-      <div className='hero-text'>
-        <h1>This is a heading</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis magnam molestiae sequi maxime quo modi ipsa, id omnis aut illum voluptatibus iure assumenda sint doloribus soluta obcaecati. Modi, quasi veniam?</p>
-        <button className='button'>Know more</button>
+        <div className='hero-text'>
+          <h1>This is a heading</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis magnam molestiae sequi maxime quo modi ipsa, id omnis aut illum voluptatibus iure assumenda sint doloribus soluta obcaecati. Modi, quasi veniam?</p>
+          <button className='button'>Know more</button>
         </div>
+        <img src={wheel} alt='Half Rotating Image' className='hero-image' />
       </div>
-      <div className="img"></div>
     </div>
   </>
   )
