14a9a8f0346dddebd3399795583d2ebc5f14ae9d
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-07 11:21:34 -0300

    added card flip functionality

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import './goals.css'
 import data from '../data/data';
 import grafica from '../assets/ODS-grafica-c.png'
+import ods from '../assets/ODS-1.png'
 
 const Goals = () => {
   return (
     <div className="main section basic-margin">
       <h2>{data.goals.h2}</h2>
       <section>
-        <img src={grafica} alt="" width={400} />
         <div className="goals">
-          images og each goal
+          <div className="card">
+            <div className="front">
+              <img src={ods} alt="" />
+            </div>
+            <div className="back">
+              <div className="info">
+              <p>Accede al link para conocer mas</p>
+              <a href="#">ODS 1</a></div>
+
+
+            </div>
+          </div>
+
         </div>
+        <img src={grafica} alt="" width={400} />
       </section>
 
     </div>
