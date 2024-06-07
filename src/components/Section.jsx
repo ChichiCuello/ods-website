6fc565201e739dffe38aaba29a0c6f208868cadc
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-07 09:43:35 -0300

    added functionality to play buttons

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import './section.css'
 import data from '../data/data'
-import odsGrafica from '../assets/ODS-grafica-1.png'
+import odsGraficaA from '../assets/ODS-grafica-a.png';
+import odsGraficaB from '../assets/ODS-grafica-b.png';
+import { FaRegPlayCircle } from "react-icons/fa";
 
 const Section = () => {
   return (<>
   <div className="main section basic-margin">
     <div className='flex-container'>
-      <div><a href="https://www.youtube.com/watch?v=MCKH5xk8X-g&t=5s "><img src={odsGrafica} alt="" width={300}/></a></div>
-      <div className="flex-item">      
+      <div className='graphic'>
+        <a href="https://www.youtube.com/watch?v=MCKH5xk8X-g&t=0s" target="_blank">
+          <FaRegPlayCircle className='play-btn-a'/>
+        </a>
+        <img src={odsGraficaA} alt="" width={300}/>
+      </div>
+      <div className="flex-item">
       <h2>{data.sectionOne.h2}</h2>
       <p>{data.sectionOne.p}</p>
       </div>
     </div>
     <div className='flex-container second'>
-    <div><a href="https://www.youtube.com/watch?v=MCKH5xk8X-g&t=5s "><img src={odsGrafica} alt="" width={300}/></a></div>
-      <div className="flex-item">      
+    <div  className='graphic' ><a href="https://www.youtube.com/watch?v=OK_8zOAJJPE&t=0s" target="_blank">
+      <FaRegPlayCircle className='play-btn-b'/></a>
+      <img src={odsGraficaB} alt="" width={300}/></div>
+      <div className="flex-item">
       <h2>{data.sectionTwo.h2}</h2>
       <p>{data.sectionTwo.p.one}</p>
       <p>{data.sectionTwo.p.two}</p>
