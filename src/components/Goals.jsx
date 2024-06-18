b42d04ac473b0692fee9661bac696fcb92535712
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-06-18 16:36:06 -0300

    added styles for cards

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './goals.css'
 import data from '../data/data';
 import grafica from '../assets/ODS-grafica-c.png'
-import ods from '../assets/ODS-1.png'
-
 const Goals = () => {
+
+  const goals = Array.from({ length: 17 }, (_, i) => i + 1);
+
   return (
     <div className="main section basic-margin">
       <h2>{data.goals.h2}</h2>
-      <section>
+      <section className='section-goals'>
         <div className="goals">
-          <div className="card">
-            <div className="front">
-              <img src={ods} alt="" />
-            </div>
-            <div className="back">
-              <div className="info">
-              <p>Accede al link para conocer mas</p>
-              <a href="#">ODS 1</a></div>
-
-
-            </div>
-          </div>
+        {goals.map(goal => {
+                // Dynamically import the image based on the goal number
+                const odsImage = require(`../assets/ODS/ODS-${goal}.png`);
+                return (
+                  <div className="card" key={goal}>
+                    <div className="front">
+                    <Link to={`ods/${goal}`}><img src={odsImage} alt={`ODS ${goal}`} /></Link>
+                    </div>
+                    <div className="back">
+                      <div className="info">
+                        <p>Accede al link para conocer más</p>
+                        <Link to={`ods/${goal}`}>ODS {goal}</Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
 
         </div>
-        <img src={grafica} alt="" width={400} />
+        <img className="girl-img" src={grafica} alt="" width={400} />
       </section>
-
     </div>
   )
 }
