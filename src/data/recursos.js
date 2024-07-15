0e697f1363ea7427c21fb9822dc85142a9cf3a71
Chichi Cuello <ma.agustina.cuello@gmail.com>
2024-07-15 17:47:35 -0300

    added styles to recursos

---
+++ src/data/recursos.js
@@ -0,0 +1,9 @@
+const recursos = {
+  videos: {
+  link:'https://chatgpt.com/c/e02e2a95-649e-4cdd-a715-726c2d0a63f5',
+  title: 'ODS: ¿cómo aplicarlos en tu día a día?',
+  p: '¿Sabías que sos una pieza clave en la construcción de un futuro mejor? Conocé los 17 objetivos de desarrollo sostenible de la Agenda 2030 y accedé a recursos educativos que te ayudarán a hacer la diferencia desde tu casa, tu trabajo y tu comunidad.',
+  },
+}
+
+export default recursos;
--- src/routes/Recursos.jsx
+++ src/routes/Recursos.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './recursos.css'
+import '../components/navbar.css'
+import recursos from '../data/recursos'
 
 const Recursos = () => {
   return (
-      <div>
-        <h1>Recursos</h1>  {/* Add your desired content here */}
-        <p>This is the Recursos page. Add more content as needed.</p>
+      <div className='recursos basic-margin'>
+        <h2 className='slide-in-left'>Videos Educativos</h2>
+        <h2 className='slide-in-right'>Articulos</h2>
+        <h2 className='slide-in-left'>Juegos</h2>
+        <h2 className='slide-in-right'>Trivia</h2>
+        <h2 className='slide-in-left'>Links a páginas oficiales</h2>
+
+
+        <section>
+          <a className = 'link' href={recursos.videos.link} target='_blank'>{recursos.videos.title}</a>
+          <p>{recursos.videos.p}</p>
+        </section>
       </div>
 
   )
